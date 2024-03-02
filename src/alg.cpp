de3899c8ec4442316e8623cf2534ee55258d32ed
xQiwix <tsiganovkirill@yandex.ru>
2024-03-02 15:26:03 +0300

    ADS1 b176

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,43 @@
 
 
 bool checkPrime(uint64_t value) {
-     
+    if(value <= 1){
+        return false;
+    }
+    for(int i = 2;i<=value;i++){
+        if(value%i==0){
+            return false;
+        }
+        return true;
+    }
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+uint64_t chesl = 2,cont = 1;
+    while(cont<n){
+        chesl+=1;
+    if(checkPrime(chesl)){
+        cont+=1;
+        }
+    }
+    return chesl;
+
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    value += 1;
+    while (!checkPrime(value)) {
+        value += 1;
+    }
+    return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    uint64_t sum = 0;
+    for (uint64_t i = 2; i < hbound; i ++) {
+        if (checkPrime(i)) {
+            sum += i;
+        }
+    }
+    return sum;
 }
